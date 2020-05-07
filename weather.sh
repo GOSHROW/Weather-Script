d13b767b778290181cf46519e773496edfbc04ec
GOSHROW <abhroindia@gmail.com>
2020-05-08 01:39:44 +0530

    Options added

--- weather.sh
+++ weather.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
-#CLI on igor_chubin project
+#CLI on igor_chubin's project
 
 url="http://wttr.in"
-url="${url}/${1}"
+
+len=0
+
+if [[ $1 == -* ]]; then
+    if [[ $1 == "-b" ]]; then
+        len=7
+    elif [[ $1 == "-t" ]]; then
+        len=17
+    elif [[ $1 == "-e" ]]; then
+        len=0
+    elif [[ $1 == "-l" ]]; then
+        len=-1
+    else
+        echo "Invalid"
+        exit
+    fi
+    url="${url}/${2}"
+else
+    url="${url}/${1}"
+fi
 
 weather=`curl -s $url`
-touch tempfile
-echo "$weather" >> tempfile
+# echo $weather
+touch tempweatherfile
+echo "$weather" >> tempweatherfile
 
-head -7 tempfile
+if [[ $len == 0 ]]; then
+    cat tempweatherfile
+elif [[ $len == -1 ]]; then
+    tail -3 tempweatherfile
+else
+    head -$len tempweatherfile
+fi
+
+rm tempweatherfile
